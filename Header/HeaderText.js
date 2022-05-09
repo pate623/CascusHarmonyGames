0c0bdcebaccc6f102348e9d8f67ebf5a583ae58c
pate623 <53486622+pate623@users.noreply.github.com>
2022-05-09 09:33:43 +0300

    have to use camel case for html page names

--- Header/HeaderText.js
+++ Header/HeaderText.js
@@ -1,7 +1,7 @@
 if(IsMobile){
     document.write(`
         <header class="Header">
-            <a href="Index.html"><div class="HeaderLogo_Mobile"></div></a>
+            <a href="index.html"><div class="HeaderLogo_Mobile"></div></a>
             <div class="HeaderBurger_Mobile">
                 <div class="HeaderMenuLine_Mobile"></div>
                 <div class="HeaderMenuLine_Mobile"></div>
@@ -11,7 +11,7 @@ if(IsMobile){
         </header>
 
         <div class="ScrolledHeader">
-            <a href="Index.html"><div class="HeaderLogo_Mobile"></div></a>
+            <a href="index.html"><div class="HeaderLogo_Mobile"></div></a>
             <div class="HeaderBurger_Mobile HeaderBurger_MobileScroll">
                 <div class="HeaderMenuLine_Mobile"></div>
                 <div class="HeaderMenuLine_Mobile"></div>
@@ -21,19 +21,19 @@ if(IsMobile){
         </div>
 
         <div class="HeaderLinks_Mobile">
-            <a href="Index.html"><div class="HeaderText_Mobile">Home</div></a>
-            <a href="KuiperBelt.html"><div class="HeaderText_Mobile">Defender of Kuiper Belt</div></a>
-            <a href="BattleMagicians.html"><div class="HeaderText_Mobile">Battle Magicians (WIP)</div></a>
+            <a href="index.html"><div class="HeaderText_Mobile">Home</div></a>
+            <a href="kuiperBelt.html"><div class="HeaderText_Mobile">Defender of Kuiper Belt</div></a>
+            <a href="battleMagicians.html"><div class="HeaderText_Mobile">Battle Magicians (WIP)</div></a>
         </div>
     `);
 }else{ // Desktop
     document.write(`
         <header class="Header">
-            <a href="Index.html"><div id="HeaderLogo"></div></a>
+            <a href="index.html"><div id="HeaderLogo"></div></a>
             <div id="HeaderLinks">
-                <a href="Index.html"><div class="HeaderText">Home</div></a>
-                <a href="KuiperBelt.html"><div class="HeaderText">Defender of Kuiper Belt</div></a>
-                <a href="BattleMagicians.html"><div class="HeaderText">Battle Magicians</div></a>
+                <a href="index.html"><div class="HeaderText">Home</div></a>
+                <a href="kuiperBelt.html"><div class="HeaderText">Defender of Kuiper Belt</div></a>
+                <a href="battleMagicians.html"><div class="HeaderText">Battle Magicians</div></a>
             </div>
         </header>
     `);
